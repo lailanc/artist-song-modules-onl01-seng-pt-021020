411d615b92a4817fadd639f03ba520baf0def74b
Laila Campbell <lailacampbell96@gmail.com>
2020-03-22 08:11:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,13 @@
-require_relative '../lib/concerns/memorable'
-require_relative '../lib/concerns/findable'
-require_relative '../lib/concerns/paramable'
-
-
-
 require 'pry'
 
 class Artist
   attr_accessor :name
   attr_reader :songs
-  
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
 
   @@artists = []
 
   def initialize
-    super
+    @@artists << self
     @songs = []
   end
 
@@ -29,13 +19,13 @@ class Artist
     @@artists
   end
 
-  #def self.reset_all
-    #self.all.clear
-  #end
+  def self.reset_all
+    self.all.clear
+  end
 
-  #def self.count
-    #self.all.count
-  #end
+  def self.count
+    self.all.count
+  end
 
   def add_song(song)
     @songs << song
@@ -46,5 +36,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
